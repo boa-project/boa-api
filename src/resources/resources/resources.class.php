d86e147c677f41ebd35fe25b406dc3b944b5166a
cirano <davidherney@gmail.com>
2020-09-04 22:09:53 -0500

    Resources validations

--- src/resources/resources/resources.class.php
+++ src/resources/resources/resources.class.php
@@ -82,16 +82,21 @@ class Resources extends ComplexObjectList {
 
         foreach($list as $key => $one) {
 
-            // Replace the single by full manifest.
-            $fullone = new Resource($one->catalog_id, base64_encode($one->id));
-            $one->manifest = $fullone->manifest;
-            $one->social = $fullone->social;
-            $one->about = Restos::URIRest('c/' . $one->catalog_id . '/resources/' . base64_encode($one->id));
+            try {
+                // Replace the single by full manifest.
+                $fullone = new Resource($one->catalog_id, base64_encode($one->id));
+                $one->manifest = $fullone->manifest;
+                $one->social = $fullone->social;
+                $one->about = Restos::URIRest('c/' . $one->catalog_id . '/resources/' . base64_encode($one->id));
 
-            /*if (property_exists($one->manifest, 'customicon') && !empty($one->manifest->customicon)) {
-                // The customicon name is replaced by the specific object image uri. Only if customicon exists.
-                $one->manifest->customicon = Restos::URIRest('c/' . $one->catalog_id . '/resources/' . base64_encode($one->id) . '.img');
-            }*/
+                /*if (property_exists($one->manifest, 'customicon') && !empty($one->manifest->customicon)) {
+                    // The customicon name is replaced by the specific object image uri. Only if customicon exists.
+                    $one->manifest->customicon = Restos::URIRest('c/' . $one->catalog_id . '/resources/' . base64_encode($one->id) . '.img');
+                }*/
+
+            } catch (Exception $e) {
+                unset($list[$key]);
+            }
         }
 
         return $list;
